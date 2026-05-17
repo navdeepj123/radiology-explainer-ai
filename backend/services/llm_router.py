2d89aa538e9348a6c02e271ff0fc427b238e6916
Hunit01 <hunitkamboj072@gmail.com>
2026-05-18 11:37:12 +1200

    Added openai

--- backend/services/llm_router.py
+++ backend/services/llm_router.py
@@ -1,13 +1,21 @@
 from services.ollama_service import generate_with_ollama
 from services.groq_service import generate_with_groq
+from services.openai_service import generate_with_openai
 
 def generate_with_provider(prompt, provider="ollama"):
+
     try:
+
         if provider == "groq":
             return generate_with_groq(prompt)
+
+        elif provider == "openai":
+            return generate_with_openai(prompt)
 
         return generate_with_ollama(prompt)
 
     except Exception as e:
+
         print("Provider failed, using Ollama:", e)
+
         return generate_with_ollama(prompt)
